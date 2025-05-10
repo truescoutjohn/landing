b58806a96140ae9832a387181cd773282c36e10d
Sasha Tkachenko <sashatkachenkobusiness@gmail.com>
2025-05-10 09:25:32 +0300

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,10 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- @vite(['resources/js/app.ts']) -->
-  <link rel="stylesheet" href="https://bold-mildly-kite.ngrok-free.app/build/assets/app-B0UCj5Lw.css">
+  @vite(['resources/js/app.ts'])
 </head>
 <body>
   <div id="app">
   </div>
-  <script src="https://bold-mildly-kite.ngrok-free.app/build/assets/app-C6mNdPb7.js"></script>
 </body>
 </html>
