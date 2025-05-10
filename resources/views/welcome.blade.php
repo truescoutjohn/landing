199239366386981842f1e00848c571018e0d60af
Sasha Tkachenko <sashatkachenkobusiness@gmail.com>
2025-05-10 13:29:31 +0300

    Fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,12 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  @vite(['resources/js/app.ts'])
+  <link rel="stylesheet" href="{{ asset('app-B0UCj5Lw.css') }}">
 </head>
 <body>
   <div id="app">
   </div>
+  <script src="{{ asset('app-C6mNdPb7.js') }}"></script>
+  
 </body>
 </html>
