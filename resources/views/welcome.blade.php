ed7ca1c94c1a75422a0880d044147962ce3f1728
Sasha Tkachenko <sashatkachenkobusiness@gmail.com>
2025-05-12 17:18:43 +0300

    fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,11 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  @vite(['resources/js/app.ts'])
-  <!-- <link rel="stylesheet" href="http://3175229.pt582771.web.hosting-test.net/build/assets/app-B0UCj5Lw.css"> -->
+  <link rel="stylesheet" href="http://3175229.pt582771.web.hosting-test.net/build/assets/app-CdX2PR3L.css">
 </head>
 <body>
   <div id="app">
   </div>
-  <!-- <script src="http://3175229.pt582771.web.hosting-test.net/build/assets/app-C6mNdPb7.js"></script> -->
+  <script src="http://3175229.pt582771.web.hosting-test.net/build/assets/app-Bw2ckZqC.js"></script>
 </body>
 </html>
